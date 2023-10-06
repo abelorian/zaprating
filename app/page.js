735c6d4e3d94f8e70a078b65af088a06d85f0a18
Abel O'Rian <abel.orian@gmail.com>
2023-10-06 11:29:40 -0300

    Add: get rating data

--- app/page.js
+++ app/page.js
@@ -1,95 +1,69 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { TwitterApi } from 'twitter-api-v2';
+import * as cheerio from 'cheerio';
 
-export default function Home() {
+async function fetchData(key) {
+  try {
+    const response = await fetch(`https://metrics.zappingtv.com/public/rating/${key}`);
+
+    if (!response.ok) {
+      return null
+    }
+
+    const htmlText = await response.text();
+    const $ = cheerio.load(htmlText);
+    const channelRatingElement = $('#channel_rating');
+
+    if (channelRatingElement.length > 0) {
+      const channelRating = channelRatingElement.text();
+      return channelRating
+    } else {
+      console.log('No se encontró el elemento con el id "channel_rating"');
+    }
+
+  } catch (error) {
+    return null
+  }
+}
+
+export default async function Home() {
+  const canal13 = await fetchData("13")
+  const tvn = await fetchData("tvno")
+  const lared = await fetchData("lared")
+  const mega = await fetchData("mega")
+  const chv = await fetchData("chv")
+  const tvm = await fetchData("tvm")
+
+  //tweet()
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <p>Canal 13: { canal13 }</p>
+      <p>TVN: { tvn }</p>
+      <p>La Red: { lared }</p>
+      <p>Mega: { mega }</p>
+      <p>Chilevision: { chv }</p>
+      <p>TV+: { tvm }</p>
+    </div>
   )
+}
+
+
+async function tweet() {
+    const userClient = new TwitterApi({
+      appKey: process.env.APP_KEY,
+      appSecret: process.env.APP_SECRET,
+      accessToken: process.env.ACCESS_TOKEN,
+      accessSecret: process.env.ACCESS_SECRET,
+    });
+
+    const me = await userClient.v2.me()
+
+    console.log("*******************************************")
+    console.log(me)
+
+    if (false) {
+      await userClient.v2.tweet({
+        text: 'prueba'
+      });
+    }
 }
