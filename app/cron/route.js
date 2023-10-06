84470b55df1fd1c0c5e0360b67bb3366519276e3
Abel O'Rian <abel.orian@gmail.com>
2023-10-06 20:11:56 -0300

    Fix: tweet format

--- app/cron/route.js
+++ app/cron/route.js
@@ -13,7 +13,7 @@ export async function GET(request) {
   const chv = await Ratings("chv")
   const tvm = await Ratings("tvm")
 
-  Tweet(`Canal 13: ${canal13} \nTVN: ${tvn}\nMega: ${mega}\nCHV: ${chv}\nLaRed: ${lared}\nTV+: ${tvm}`)
+  Tweet(`Canal 13 → ${canal13}\nTVN → ${tvn}\nMega → ${mega}\nCHV → ${chv}\nLaRed → ${lared}\nTV+ → ${tvm}`)
 
   return NextResponse.json({ canal13, tvn, lared, mega, chv, tvm });
 }
